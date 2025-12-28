48eb00dedcc75efb9e3d4f8651b6072735a0cca7
trychiasagitaananda03 <trychiasagitaananda03@gmail.com>
2025-12-28 13:10:51 +0700

    menyelesaikan project 1

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,20 @@
-const menu = document.querySelector('.menu input');
-const nav = document.querySelector('nav ul')
+const menu = document.querySelector(".menu input");
+const nav = document.querySelector("nav ul");
 
-menu.addEventListener('click', function () {
-    nav.classList.toggle('slide');
+menu.addEventListener("click", function () {
+  nav.classList.toggle("slide");
+});
+const scriptURL = "https://script.google.com/macros/s/AKfycbx5MhV9pJjppOofqTQ4mFtL0I3tiXDAVpfhEIhAWqTOUIJvxsRcuDuOu8lQbnclC0dg6A/exec";
+const form = document.forms["contact-form"];
+const btnKirim = document.querySelector(".btn-kirim");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      console.log("Success!", response);
+      //reset formnya
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
 });
